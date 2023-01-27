2a7e67dc4b1d5ae0a39f3b729cc4ebe185136b81
Ashishbachan14 <120729921+Ashishbachan14@users.noreply.github.com>
2023-01-27 19:51:18 +0530

    Update q7.py

--- q7.py
+++ q7.py
@@ -2,3 +2,20 @@
 some instances and check whether they are instances of the said classes or not. Also,
 check whether the said classes are subclasses of the built-in object class or not.'''
 
+class Student:
+    pass
+
+class Marks:
+    pass
+
+Ashish=Student()
+
+Ashish_ke_marks=Marks()
+
+print("Ashish is an instance of class Student:",isinstance(Ashish,Student))
+print("Ashish is an instance of class Marks:",isinstance(Ashish,Marks))
+print("Ashish_ke_marks is an instance of class Student:",isinstance(Ashish_ke_marks,Student))
+print("Ashish_ke_marks is an instance of class Marks:",isinstance(Ashish_ke_marks,Marks))
+
+print("Student is a subclass of class object:",issubclass(Student,object))
+print("Marks is a subclass of class object:",issubclass(Marks,object))
