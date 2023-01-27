b1f76f30b3c4e89d6e05cc363ecf32df88c73668
Ashishbachan14 <120729921+Ashishbachan14@users.noreply.github.com>
2023-01-27 19:44:37 +0530

    Update q6_student_data.py

--- q6_student_data.py
+++ q6_student_data.py
@@ -2,3 +2,14 @@
 If the user passes an argument student_name or student_class the function will print the
 student name and class.'''
 
+def student_data(student_id,**kwargs):
+    print("Student ID is ",student_id)
+    if 'student_name' in kwargs:
+        print("Student name is ",kwargs['student_name'])
+    if 'student_class' in kwargs:
+        print("Student class is ",kwargs['student_class'])
+    print()
+
+student_data(student_id="22106001")
+student_data(student_id="22106001", student_name="Sambhav Jain")
+student_data(student_id="22106001", student_name="Sambhav Jain", student_class="1st Year")
